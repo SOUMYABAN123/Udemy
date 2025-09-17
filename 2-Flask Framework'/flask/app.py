fbef96438355cdb3d3856518a63452e85f0a8fa0
SOUMYABAN123 <banerjeesoumyasekhar@gmail.com>
2025-09-17 03:24:07 +0200

    commit all files

--- 2-Flask Framework'/flask/app.py
+++ 2-Flask Framework'/flask/app.py
@@ -1,1 +1,25 @@
-import flask
+from flask import Flask, render_template
+
+## intializing the flask app, 
+## means it will create an instance of Flask class which will be your WSGI application
+app = Flask(__name__)
+
+@app.route("/")        ##decorator= as soon as the app instance go to route with the / symbol it will go the defination function
+def welcome():
+    return "Welcome to the Flask course. This should be an amazing course"
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ =="__main__":
+    app.run(debug = True)
+
+
+
+
